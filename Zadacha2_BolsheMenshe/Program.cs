6929b8821136c70a7189f8607aa0416f85e5cc31
AlexandrVladlenovich <Mystafin_Alexandr@mail.ru>
2022-08-09 22:51:21 +0500

    add Zadacha2_BolsheMenshe

---
+++ Zadacha2_BolsheMenshe/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Введите number_A");
+string number_As = Console.ReadLine() ??"";
+int number_Ai;
+Console.WriteLine("Введите number_B");
+String number_Bs = Console.ReadLine() ??"";
+int number_Bi;
+if (int.TryParse(number_As, out number_Ai))
+{
+    if (int.TryParse(number_Bs, out number_Bi))
+    {
+        if (number_Ai < number_Bi)
+        {
+            Console.WriteLine("Число B больше");
+        }
+        else 
+        {
+            Console.WriteLine("Число А больше");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Ошибка ввода, введите числовое значение");
+    }
+}
