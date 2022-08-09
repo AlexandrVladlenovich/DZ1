09c04c5a6545107a0d81523ad2325595f46d6e24
AlexandrVladlenovich <Mystafin_Alexandr@mail.ru>
2022-08-10 03:10:42 +0500

    add Zadacha4_MaxChislo

---
+++ Zadacha4_MaxChislo/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите number_A");
+string number_As = Console.ReadLine() ??"";
+int number_Ai;
+Console.WriteLine("Введите number_B");
+String number_Bs = Console.ReadLine() ??"";
+int number_Bi;
+Console.WriteLine("Введите number_C");
+String number_Cs = Console.ReadLine() ??"";
+int number_Ci;
+
+int.TryParse(number_As, out number_Ai);
+int.TryParse(number_Bs, out number_Bi);
+int.TryParse(number_Cs, out number_Ci);
+
+int max = number_Ai;
+if (number_Bi > max) max = number_Bi;
+if (number_Ci > max) max = number_Ci;
+
+Console.Write("максимальное число= ");
+Console.WriteLine(max);
