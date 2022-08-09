c1c12a88ccd7f05e512c0183cff0c4f4c6179708
AlexandrVladlenovich <Mystafin_Alexandr@mail.ru>
2022-08-10 04:30:35 +0500

    add Zadacha8_DiapozonChetn

---
+++ Zadacha8_DiapozonChetn/Program.cs
@@ -0,0 +1,10 @@
+﻿ int a = 1;
+ int c = 2;
+ 
+Console.WriteLine("Введите число n");
+String ns = Console.ReadLine() ??"";
+int n;
+if (int.TryParse(ns, out n))
+for (int i = a; i <= n; i++)      // идет от а до n с шагом 1
+    if (i % c == 0)                // каждое чиcло делим на с , берем остаток от деления и сравниваем его с нулем
+    Console.Write(i + ";");   // если равно нулю выводим
